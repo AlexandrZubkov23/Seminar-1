3b5b5726e826780835784e22346bd710907ea921
Aleks <aleksdante2308gmail.com>
2022-10-04 16:26:09 +0300

    04.10.22

---
+++ #2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и 
+//выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+string Firstnum = Console.ReadLine() ??" ";
+int a = int.Parse(Firstnum);
+
+Console.Write("Введите второе число: ");
+string Firstnum1 = Console.ReadLine() ??" ";
+int b = int.Parse(Firstnum1);
+
+if (a > b)
+{
+    Console.Write(+ a + " большее " + b + " меньшее");
+    
+}
+else 
+{
+    Console.Write(+ b + " большее " + a + " меньшее");
+}
---
+++ #4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+string Num1 = Console.ReadLine() ?? " ";
+int a = int.Parse(Num1);
+
+Console.Write("Введите второе число: ");
+string Num2 = Console.ReadLine() ?? " ";
+int b = int.Parse(Num2);
+
+Console.Write("Введите третье число: ");
+string Num3 = Console.ReadLine() ?? " ";
+int c = int.Parse(Num3);
+int max = 0;
+
+if (a > max)
+{
+    a = max;
+}
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.Write(max);
